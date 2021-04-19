652cfdb1132e6fece1276ee2f24b1cfca8dbd4ea
LaraSellesVidal <lara.selles12@imperial.ac.uk>
2021-04-19 11:04:31 +0200

    fix warning

--- R/hpop_acceleration.R
+++ R/hpop_acceleration.R
@@ -294,7 +294,7 @@ dragAcceleration <- function(dens, r, v, T, area, mass, CD, Omega) {
 relativity <- function(r, v) {
     r_Sat <- sqrt(sum(r^2))
     v_Sat <- sqrt(sum(v^2))
-    a <- GM_Earth/(c_light^2*r_Sat^3)*((4*GM_Earth/r_Sat-v_Sat^2)*r+4*(r %*% v)*v)
+    a <- GM_Earth/(c_light^2*r_Sat^3)*((4*GM_Earth/r_Sat-v_Sat^2)*r+4*as.vector((r %*% v))*v)
     return(a)
 }
 
